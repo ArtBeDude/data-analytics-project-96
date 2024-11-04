c52699c616e23f1bfd292b0141bbeea1249ff372
ArtBeDude <arturshavlinsky01@gmail.com>
2024-11-04 13:48:33 +0300

    Модель LPC

---
+++ aggregate_last_paid_click.sql
@@ -0,0 +1,193 @@
+INSERT INTO "WITH cte1 AS (
+    SELECT
+        le.visitor_id,
+        ses.visit_date,
+        ses.source AS utm_source,
+        ses.medium AS utm_medium,
+        ses.campaign AS utm_campaign,
+        le.lead_id,
+        le.created_at,
+        le.amount,
+        le.closing_reason,
+        le.status_id,
+        ROW_NUMBER()
+            OVER (PARTITION BY ses.visitor_id ORDER BY visit_date DESC)
+        AS rn
+    FROM sessions AS ses
+    LEFT JOIN leads AS le
+        ON
+            ses.visitor_id = le.visitor_id
+            AND ses.visit_date <= le.created_at
+    WHERE ses.medium <> 'organic'
+),
+cte2 AS (
+    SELECT
+        visitor_id,
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id
+    FROM cte1
+    WHERE rn = 1
+    ORDER BY
+        amount DESC NULLS LAST,
+        visit_date ASC,
+        utm_source ASC,
+        utm_medium ASC,
+        utm_campaign ASC
+),
+ya_vk_spent AS (
+    SELECT
+        TO_CHAR(campaign_date, 'YYYY-MM-DD') AS date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+    GROUP BY 1, 2, 3, 4
+    UNION ALL
+    SELECT
+        TO_CHAR(campaign_date, 'YYYY-MM-DD') AS date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY 1, 2, 3, 4
+    ORDER BY 1
+)
+SELECT
+    TO_CHAR(cte2.visit_date, 'YYYY-MM-DD') AS visit_date,
+    cte2.utm_source,
+    cte2.utm_medium,
+    cte2.utm_campaign,
+    yv.total_cost,
+    COUNT(*) AS visitors_count,
+    COUNT(*) FILTER (WHERE lead_id IS NOT NULL) AS leads_count,
+    COUNT(*) FILTER (WHERE status_id = 142) AS purchases_count,
+    SUM(amount) AS revenue
+FROM cte2
+LEFT JOIN ya_vk_spent AS yv -- 
+    ON
+        TO_CHAR(cte2.visit_date, 'YYYY-MM-DD') = yv.date
+        AND cte2.utm_source = yv.utm_source
+        AND cte2.utm_medium = yv.utm_medium
+        AND cte2.utm_campaign = yv.utm_campaign
+GROUP BY 1, 2, 3, 4, 5
+ORDER BY
+    revenue DESC NULLS LAST,
+    visit_date ASC,
+    visitors_count DESC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
+LIMIT 15" (visit_date,utm_source,utm_medium,utm_campaign,total_cost,visitors_count,leads_count,purchases_count,revenue) VALUES
+	 ('2023-06-01','yandex','cpc','freemium',21654,103,100,25,1654810),
+	 ('2023-06-01','yandex','cpc','prof-frontend',57138,78,73,12,1057000),
+	 ('2023-06-01','yandex','cpc','prof-python',33026,62,61,7,613196),
+	 ('2023-06-01','yandex','cpc','base-python',12524,31,28,7,476021),
+	 ('2023-06-01','vk','cpc','prof-python',2028,71,40,5,355564),
+	 ('2023-06-01','vk','cpc','freemium-frontend',3160,80,43,5,338083),
+	 ('2023-06-01','yandex','cpc','prof-java',29490,49,49,4,284590),
+	 ('2023-06-01','yandex','cpc','base-frontend',18061,40,39,3,268515),
+	 ('2023-06-01','vk','cpc','prof-java',3115,51,23,2,232726),
+	 ('2023-06-01','yandex','cpc','prof-data-analytics',10086,14,14,2,220264);
+INSERT INTO "WITH cte1 AS (
+    SELECT
+        le.visitor_id,
+        ses.visit_date,
+        ses.source AS utm_source,
+        ses.medium AS utm_medium,
+        ses.campaign AS utm_campaign,
+        le.lead_id,
+        le.created_at,
+        le.amount,
+        le.closing_reason,
+        le.status_id,
+        ROW_NUMBER()
+            OVER (PARTITION BY ses.visitor_id ORDER BY visit_date DESC)
+        AS rn
+    FROM sessions AS ses
+    LEFT JOIN leads AS le
+        ON
+            ses.visitor_id = le.visitor_id
+            AND ses.visit_date <= le.created_at
+    WHERE ses.medium <> 'organic'
+),
+cte2 AS (
+    SELECT
+        visitor_id,
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id
+    FROM cte1
+    WHERE rn = 1
+    ORDER BY
+        amount DESC NULLS LAST,
+        visit_date ASC,
+        utm_source ASC,
+        utm_medium ASC,
+        utm_campaign ASC
+),
+ya_vk_spent AS (
+    SELECT
+        TO_CHAR(campaign_date, 'YYYY-MM-DD') AS date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+    GROUP BY 1, 2, 3, 4
+    UNION ALL
+    SELECT
+        TO_CHAR(campaign_date, 'YYYY-MM-DD') AS date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY 1, 2, 3, 4
+    ORDER BY 1
+)
+SELECT
+    TO_CHAR(cte2.visit_date, 'YYYY-MM-DD') AS visit_date,
+    cte2.utm_source,
+    cte2.utm_medium,
+    cte2.utm_campaign,
+    yv.total_cost,
+    COUNT(*) AS visitors_count,
+    COUNT(*) FILTER (WHERE lead_id IS NOT NULL) AS leads_count,
+    COUNT(*) FILTER (WHERE status_id = 142) AS purchases_count,
+    SUM(amount) AS revenue
+FROM cte2
+LEFT JOIN ya_vk_spent AS yv -- 
+    ON
+        TO_CHAR(cte2.visit_date, 'YYYY-MM-DD') = yv.date
+        AND cte2.utm_source = yv.utm_source
+        AND cte2.utm_medium = yv.utm_medium
+        AND cte2.utm_campaign = yv.utm_campaign
+GROUP BY 1, 2, 3, 4, 5
+ORDER BY
+    revenue DESC NULLS LAST,
+    visit_date ASC,
+    visitors_count DESC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
+LIMIT 15" (visit_date,utm_source,utm_medium,utm_campaign,total_cost,visitors_count,leads_count,purchases_count,revenue) VALUES
+	 ('2023-06-01','yandex','cpc','prof-professions-brand',4818,13,13,3,154287),
+	 ('2023-06-20','telegram','cpp','base-java',NULL,3,1,1,151192),
+	 ('2023-06-01','yandex','cpc','dod-php',5964,4,4,1,150255),
+	 ('2023-06-01','yandex','cpc','base-professions-retarget',151,4,4,1,134100),
+	 ('2023-06-07','vk','social','hexlet-blog',NULL,3,1,1,84000);
