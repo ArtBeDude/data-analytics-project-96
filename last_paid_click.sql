a0fb0c392022082f2dbd92b088f008346691ecd1
ArturShavlinsky <ArturShavlinsky01@gmail.com>
2024-11-10 12:30:13 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -11,8 +11,9 @@ WITH cte1 AS (
         le.closing_reason,
         le.status_id,
         ROW_NUMBER()
-            OVER (PARTITION BY ses.visitor_id ORDER BY ses.visit_date DESC)
-        AS rn
+            OVER (
+                PARTITION BY ses.visitor_id ORDER BY ses.visit_date DESC
+        ) AS rn
     FROM sessions AS ses
     LEFT JOIN leads AS le
         ON
